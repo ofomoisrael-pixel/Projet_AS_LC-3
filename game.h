143c5cfe1166508dd025fe63d235f1427c05df10
maboubakar369-tech <maboubakar369@gmail.com>
2026-06-01 16:21:07 +0200

    game.c et game.h

--- game.h
+++ game.h
@@ -4,21 +4,38 @@
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
 
-typedef struct {
+typedef struct
+{
+    int x;
+    int y;
     int shape[4][4];
-    int x, y;
 } Piece;
 
-typedef struct {
+typedef struct
+{
     int board[BOARD_HEIGHT][BOARD_WIDTH];
     Piece current;
-    int gameOver;
     int score;
+    int gameOver;
+    int victory; // 0 = en cours/perdu, 1 = gagné
 } Game;
 
+// Fonctions d'initialisation et de cycle de vie
 void initGame(Game *game);
-int movePiece(Game *game, int dx, int dy);
+void spawnPiece(Game *game);
+void checkGameStatus(Game *game);
+
+// Fonctions de mouvements du joueur
+void movePiece(Game *game, int dx, int dy);
 void rotatePiece(Game *game);
 void hardDrop(Game *game);
+
+// Fonctions logiques internes du jeu
+void lockPiece(Game *game);
+void clearLines(Game *game);
+int collision(Game *game, int x, int y);
+
+// Fonction d'interaction multijoueur (Malus)
+void addGarbage(Game *game, int lines);
 
 #endif
