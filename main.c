c316db1947f5c1f4db7d147c87a5fb92b271e1f4
Shayton <ofomoisrael@gmail.com>
2026-06-01 15:56:05 +0200

    Attempt

--- main.c
+++ main.c
@@ -1,42 +1,26 @@
 #include "SDL.h"
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
-
 #include "game.h"
 #include "graphics.h"
 #include "network.h"
 
 Game game;
 
-void afficherTuto() {
-    printf("\n=== TUTORIEL TETRIS BATTLE ===\n");
-    printf("- FLECHES : Deplacer / Tourner\n");
-    printf("- ESPACE  : Chute rapide\n");
-    printf("==============================\n\n");
-}
-
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     char ip[16];
+    printf("IP Serveur : ");
+    scanf("%15s", ip);
 
-    printf("Entrez l'IP du serveur : ");
-    if (scanf("%15s", ip) != 1) return 1;
-
-    if (!networkInit(ip)) {
-        printf("Erreur de connexion reseau.\n");
-        return 1;
-    }
-
+    if (!networkInit(ip)) return 1;
     if (!initGraphics()) return 1;
 
-    afficherTuto();
     initGame(&game);
-    
     int running = 1;
     SDL_Event event;
     Uint32 lastFall = SDL_GetTicks();
-    Uint32 startTime = SDL_GetTicks(); // Debut du chrono
+    Uint32 startTime = SDL_GetTicks();
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -53,21 +37,25 @@ int main(int argc, char *argv[]) {
         }
 
         receivePackets();
-
-        int timeInSeconds = (SDL_GetTicks() - startTime) / 1000;
+        int timePassed = (SDL_GetTicks() - startTime) / 1000;
 
         if (!game.gameOver) {
-            Uint32 now = SDL_GetTicks();
-            if (now - lastFall > 500) {
-                movePiece(&game, 0, 1);
-                lastFall = now;
+            if (SDL_GetTicks() - lastFall > 500) {
+                if (!movePiece(&game, 0, 1)) {
+                    // Si on ne peut plus descendre, vérifier lignes, etc.
+                }
+                lastFall = SDL_GetTicks();
             }
+            sendUpdate(); // Envoie les données aux autres
+        } else {
+            sendGameOver();
         }
-        
-        renderGame(&game, timeInSeconds);
+
+        renderGame(&game, timePassed);
         SDL_Delay(16);
     }
-    
+
+    networkClose();
     cleanupGraphics();
     return 0;
 }
--- network.c
+++ network.c
@@ -1,38 +1,52 @@
+#include "network.h"
+#include "game.h"
+#include <stdio.h>
+
 #ifdef _WIN32
     #include <winsock2.h>
-    #include <ws2tcpip.h> // Indispensable pour inet_pton
+    #include <ws2tcpip.h>
 #else
     #include <arpa/inet.h>
     #include <unistd.h>
+    #define ioctlsocket ioctl
 #endif
-#include "SDL.h" // Ou "SDL2/SDL.h" selon ton dossier
-#include <stdio.h>
-#include "network.h"
-#include "game.h"
-
-#pragma comment(lib, "ws2_32.lib")
 
 extern Game game;
 static int sockfd;
 static struct sockaddr_in serverAddr;
 int myPlayerId = -1;
+int playersAlive[MAX_PLAYERS] = {0, 0, 0, 0};
 
 int networkInit(char *serverIp) {
+    #ifdef _WIN32
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
+    #endif
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(5000);
+    serverAddr.sin_port = htons(SERVER_PORT);
     inet_pton(AF_INET, serverIp, &serverAddr.sin_addr);
 
-    // Socket non-bloquant
     unsigned long mode = 1;
     ioctlsocket(sockfd, FIONBIO, &mode);
 
-    Packet join = {PACKET_JOIN, 0, 0};
+    Packet join = {PACKET_JOIN, -1, 0, {0}};
     sendto(sockfd, (char*)&join, sizeof(join), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
     return 1;
+}
+
+void sendUpdate(void) {
+    if (myPlayerId == -1 || game.gameOver) return;
+    Packet p;
+    p.type = PACKET_UPDATE;
+    p.playerId = myPlayerId;
+    p.value = game.score;
+    for(int i=0; i<20; i++) {
+        p.boardState[i] = 0;
+        for(int j=0; j<10; j++) if(game.board[i][j]) p.boardState[i] = 1;
+    }
+    sendto(sockfd, (char*)&p, sizeof(p), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 }
 
 void receivePackets(void) {
@@ -40,17 +54,31 @@ void receivePackets(void) {
     struct sockaddr_in from;
     int len = sizeof(from);
     while (recvfrom(sockfd, (char*)&p, sizeof(p), 0, (struct sockaddr*)&from, &len) > 0) {
-        if (p.type == PACKET_ATTACK) addGarbage(&game, p.value);
         if (p.type == PACKET_JOIN) myPlayerId = p.playerId;
+        if (p.type == PACKET_UPDATE) {
+            if (p.playerId >= 0 && p.playerId < 4) playersAlive[p.playerId] = 1;
+        }
+        if (p.type == PACKET_GAMEOVER) {
+            if (p.playerId >= 0 && p.playerId < 4) playersAlive[p.playerId] = 0;
+        }
     }
 }
 
 void sendAttack(int lines) {
-    Packet p = {PACKET_ATTACK, myPlayerId, lines};
+    Packet p = {PACKET_ATTACK, myPlayerId, lines, {0}};
     sendto(sockfd, (char*)&p, sizeof(p), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 }
 
 void sendGameOver(void) {
-    Packet p = {PACKET_GAMEOVER, myPlayerId, 0};
+    Packet p = {PACKET_GAMEOVER, myPlayerId, 0, {0}};
     sendto(sockfd, (char*)&p, sizeof(p), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+}
+
+void networkClose(void) {
+    #ifdef _WIN32
+    closesocket(sockfd);
+    WSACleanup();
+    #else
+    close(sockfd);
+    #endif
 }
